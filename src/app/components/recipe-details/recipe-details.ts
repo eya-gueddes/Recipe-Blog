f57bc5d260c8a2e02ec18e6001d10364f120f544
eya <eya.gueddes@isimsf.u-sfax.tn>
2025-11-29 18:06:57 +0100

    components

---
+++ src/app/components/recipe-details/recipe-details.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-details',
+  imports: [],
+  templateUrl: './recipe-details.html',
+  styleUrl: './recipe-details.css',
+})
+export class RecipeDetails {
+
+}
